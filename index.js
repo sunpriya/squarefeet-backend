ae8ba5d091bdc73801dd6c6ddc97ee5ef4d98d1e
akhilsingla97 <akhilsingla97@gmail.com>
2018-10-11 02:02:18 +0530

    database setup completely and basic api tested successfully

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(bodyParser.json());
 
-
-//sample api
-app.get('/api/getList', (req,res) => {
-    const list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
-});
-
-app.post('/login', (req,res) => {
-    
-})
 
 // Handles any requests that don't match the ones above
 // app.get('*', (req,res) =>{
 //     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 // });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+const portNumber = process.env.PORT || 5000;
 
-console.log('App is listening on port ' + port);
+const server = app.listen(portNumber, ()=>{
+    let port = server.address().port;
+    console.log('Server listening on port '+ port);
+});
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "squarefeetusers",
+    password: "asdf1234",
+    database: "squarefeet"
+  });
+
+//sample api
+app.get('/api/getList', (req,res) => {
+    const list = ["item1", "item2", "item3"];
+    //res.json(list);
+    //console.log('Sent list of items');
+
+    con.connect((err) => {
+        if (err) throw err;
+        console.log("Connected!");
+        con.query("select * from userinfo;", function (err, result) {
+          if (err) throw err;
+          res.send(result);
+          console.log(result);
+        });
+      });
+
+});
+
+app.post('/api/login', (req,res) => {
+   let email = req.body.email;
+   query = "select email from userinfo where email = '" + email + "';"
+
+//    con.connect((err) => {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.query("select * from userinfo", function (err, result) {
+//       if (err) throw err;
+//       console.log("Database created");
+//     });
+//   });
+   
+});

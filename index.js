c6188fff8633b3ecdc96f0947c93a207d3b29d3a
akhilsingla97 <akhilsingla97@gmail.com>
2018-10-10 23:51:28 +0530

    removed nodd_modules

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+//sample api
+app.get('/api/getList', (req,res) => {
+    const list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+app.post('/login', (req,res) => {
+    
+})
+
+// Handles any requests that don't match the ones above
+// app.get('*', (req,res) =>{
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
